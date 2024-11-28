241d0583a14a28e3b18c511a9e2c439a6472d392
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-11-28 20:28:16 +0000

    second commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,30 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+john_jackson = {
+    "first_name": 'John',
+    "age": 33,
+    "lucky_numbers": [7,13,12]
+}
+
+jane_jackson = {
+    "first_name": 'Jane',
+    "age": 35,
+    "lucky_numbers": [10,14,3]
+}
+
+jimmy_jackson =  {
+    "first_name": 'Jimmy',
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(john_jackson)
+jackson_family.add_member(jane_jackson)
+jackson_family.add_member(jimmy_jackson)
+
+print('jacksons ---->', jackson_family.get_all_members())
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -23,13 +47,7 @@ def handle_invalid_usage(error):
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
-    return jsonify({
-        "endpoints": [
-            url_for('handle_hello'),
-            url_for('add_new_member'),
-            url_for('delete_member')
-        ]
-    }), 200
+    return generate_sitemap(app), 200
 
 
 @app.route('/members', methods=['GET'])
@@ -37,25 +55,38 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    eesti_perekond = eesti_perekond.get_all_members()
     response_body = {
-        "hello": "world",
+        "Jackson": "world",
         "family": members
     }
     return jsonify(response_body), 200
 
 @app.route('/members', methods=['POST'])
-def add_new_member():
-    member = request.json
-    jackson_family.add_member(member)
-    return jsonify(jackson_family.get_all_members()), 200
+def add_member():
+    data = request.json
+    jackson_family.add_member(data)
+    return jsonify({"Message": "member added successfully"}), 201
+    
 
+## elimar ->
 
-@app.route('/members/<int:id', methods=['DELETE'])
+#  try:
+#     response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+#     response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+#     data = response.json()  # Parse the JSON response
+#     print(data)
+# except requests.exceptions.RequestException as error:
+#     print("Error:", error)
+# ##
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
 def delete_member(id):
-    jackson_family.delete_member(id)
-    return jsonify(jackson_family.get_all_members()), 200
-
+    member = jackson_family.get_member(id)
+    if member in None:
+        return jsonify({"error": "Member not found"}), 404
+    else:
+        jackson_family.delete_member(id)
+        return jsonify({"done": True}), 200
 
 
 # this only runs if `$ python src/app.py` is executed
